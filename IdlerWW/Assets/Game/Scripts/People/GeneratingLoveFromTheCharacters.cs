1a12f484567da462a579f7171123daa9f4f1e478
gaudy228 <228822009900Art@gmail.com>
2025-03-09 02:41:33 +0300

    .

--- IdlerWW/Assets/Game/Scripts/People/GeneratingLoveFromTheCharacters.cs
+++ IdlerWW/Assets/Game/Scripts/People/GeneratingLoveFromTheCharacters.cs
@@ -4,14 +4,23 @@ public class GeneratingLoveFromTheCharacters : MonoBehaviour
 {
     [SerializeField] private float _timeBetweenSpawnLove;
     [SerializeField] private GameObject _panelWithLove;
+    private bool _inCoroutine = false;
     private void Start()
     {
         StartCoroutine(IntervalBetweenSpawnLove());
+    }
+    private void Update()
+    {
+        if (!_panelWithLove.activeSelf && !_inCoroutine)
+        {
+            StartCoroutine(IntervalBetweenSpawnLove());
+            _inCoroutine = true;
+        }
     }
     private IEnumerator IntervalBetweenSpawnLove()
     {
         yield return new WaitForSeconds(_timeBetweenSpawnLove);
         _panelWithLove.SetActive(true);
-        StartCoroutine(IntervalBetweenSpawnLove());
+        _inCoroutine = false;
     }
 }
--- IdlerWW/Assets/Game/Scripts/UI/UILookAt.cs
+++ IdlerWW/Assets/Game/Scripts/UI/UILookAt.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 public class UILookAt : MonoBehaviour
 {
-
     private void FixedUpdate()
     {
         transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Camera.main.transform.rotation * Vector3.up);
+        transform.LookAt(Camera.main.transform);
     }
 }
